499aacf92cd7473f46776b17c74dc5d302618b02
kop <kop@ubuntu.ubuntu-domain>
2015-11-03 11:09:46 +0700

    add devises

---
+++ db/migrate/20151103025106_create_users_roles.rb
@@ -0,0 +1,12 @@
+class CreateUsersRoles < ActiveRecord::Migration
+  def change
+
+    create_table :roles_users, :id => false do |t|
+      t.references :role
+      t.references :user
+    end
+
+    add_index :roles_users, [:role_id], :name => 'index_roles_users_on_role_id'
+    add_index :roles_users, [:user_id], :name => 'index_roles_users_on_user_id'
+  end
+end
